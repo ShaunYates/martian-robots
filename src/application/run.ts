b6e77ad75ad7dcd27e8d29fe9ebbc89f0027a24d
Shaun Yates <shauny@tfg.co.za>
2026-09-06 17:59:59 +0200

    feat: Ability to run app

---
+++ src/application/run.ts
@@ -0,0 +1,33 @@
+import {
+    formatOutput,
+    parseInput,
+  } from "../io";
+  
+  import {
+    runRobot,
+    World,
+    type RobotResult,
+  } from "../world";
+  
+  export function run(input: string): string {
+    const simulation = parseInput(input);
+  
+    const world = new World(
+      simulation.world.maxX,
+      simulation.world.maxY,
+    );
+  
+    const results: RobotResult[] = [];
+  
+    for (const robot of simulation.robots) {
+      const result = runRobot(
+        world,
+        robot.initialPosition,
+        robot.instructions,
+      );
+  
+      results.push(result);
+    }
+  
+    return formatOutput(results);
+  }
